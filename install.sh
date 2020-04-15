9c20e83c36fbb986f1aea3d73d982f921788bf43
William Murari <williammu6@gmail.com>
2020-04-15 19:46:15 -0300

    zshrc

--- install.sh
+++ install.sh
@@ -6,3 +6,4 @@ ln -sf $path/.gitconfig $HOME/.gitconfig
 ln -sf $path/init.vim $HOME/.config/nvim/init.vim
 ln -sf $path/.zshrc $HOME/.zshrc
 ln -sf $path/coc-settings.json $HOME/.config/nvim/coc-settings.json
+
