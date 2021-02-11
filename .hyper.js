2cf00baa4873d2c94782eed7aa889089aec9938e
William Murari <williammu6@gmail.com>
2021-02-11 01:16:53 -0300

    hyper config

---
+++ .hyper.js
@@ -0,0 +1,76 @@
+module.exports = {
+  config: {
+    // default font size in pixels for all tabs
+    fontSize: 13,
+
+    // font family with optional fallbacks
+    fontFamily:
+      '"Monaco"',
+
+    // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
+    cursorColor: "rgba(255, 255, 255, 0.5)",
+
+    // `BEAM` for |, `UNDERLINE` for _, `BLOCK` for █
+    cursorShape: "BLOCK",
+
+    // color of the text
+    foregroundColor: "#EEE",
+
+    // terminal background color
+    // backgroundColor: "rgba(0, 0, 0, 0.5)",
+    backgroundColor: "rgb(48, 51, 62)",
+
+    // border color (window, tabs)
+    borderColor: "#262626",
+
+    // custom css to embed in the main window
+    css: "",
+
+    // custom css to embed in the terminal window
+    termCSS: "",
+
+    // custom padding (css format, i.e.: `top right bottom left`)
+    padding: "0.75rem 1rem",
+
+    // the full list. if you're going to provide the full color palette,
+    // including the 6 x 6 color cubes and the grayscale map, just provide
+    // an array here instead of a color map object
+
+    // the shell to run when spawning a new session (i.e. /usr/local/bin/fish)
+    // if left empty, your system's login shell will be used by default
+    shell: "",
+
+    // for setting shell arguments (i.e. for using interactive shellArgs: ['-i'])
+    // by default ['--login'] will be used
+    shellArgs: ["--login"],
+
+    // for environment variables
+    env: {},
+
+    // set to false for no bell
+    bell: "SOUND",
+
+    // if true, selected text will automatically be copied to the clipboard
+    copyOnSelect: false,
+
+    // URL to custom bell
+    // bellSoundURL: 'http://example.com/bell.mp3',
+    verminal: {
+      fontFamily: 'Monaco',
+      fontSize: 13.5,
+      backgroundColor: 'rgb(41, 52, 61)'
+    }
+  },
+
+  // a list of plugins to fetch and install from npm
+  // format: [@org/]project[#version]
+  // examples:
+  //   `hyperpower`
+  //   `@company/project`
+  //   `project#1.0.1`
+  plugins: ["verminal", "hyperminimal"],
+  // in development, you can create a directory under
+  // `~/.hyperterm_plugins/local/` and include it here
+  // to load it and avoid it being `npm install`ed
+  localPlugins: [],
+};
